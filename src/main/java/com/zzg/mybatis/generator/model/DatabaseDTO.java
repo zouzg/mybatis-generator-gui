f43a53c2c54ca603010bbc1d167a3e2ca74e2623
zouzhigang <zouzhigang@jk.cn>
2016-05-09 07:49:09 +0800

    update UI

---
+++ src/main/java/com/zzg/mybatis/generator/model/DatabaseDTO.java
@@ -0,0 +1,29 @@
+package com.zzg.mybatis.generator.model;
+
+public class DatabaseDTO {
+
+	private String name;
+	private int value;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+
+}
