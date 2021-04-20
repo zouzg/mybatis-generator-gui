543a341684c44836a0600c9a0d43a93cddfffbc2
欧闻 <owen.zzg@alibaba-inc.com>
2021-04-20 23:27:54 +0800

    support jdk11

---
+++ src/main/java/com/zzg/mybatis/generator/Main.java
@@ -0,0 +1,15 @@
+package com.zzg.mybatis.generator;
+
+import javafx.application.Application;
+
+/**
+ * @author 欧闻
+ * @version $Id: Main, v 0.1 2021/4/20 欧闻 Exp $$
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Application.launch(MainUI.class);
+    }
+
+}
